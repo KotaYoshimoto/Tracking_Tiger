ae98b4d9d538788c11ca81ae63a720cae9e3e035
Y.Kouta <yoshimoto@ict.ehime-u.ac.jp>
2017-11-08 18:06:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ykouta/tracking_tiger2/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.ykouta.tracking_tiger2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Y.Kouta on 2017/11/04.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
